8be8e3e0cde4d27ebb66d153b21fb2e81d334465
yalinulloh <yalin.muslem@gmail.com>
2025-07-30 16:48:17 +0700

    update

--- app/src/main/java/com/trading/composequadranttrainingkotlin/MainActivity.kt
+++ app/src/main/java/com/trading/composequadranttrainingkotlin/MainActivity.kt
@@ -5,14 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
--- app/src/main/java/com/trading/composequadranttrainingkotlin/ui/theme/Theme.kt
+++ app/src/main/java/com/trading/composequadranttrainingkotlin/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.trading.composequadranttrainingkotlin.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
